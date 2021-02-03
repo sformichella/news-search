3c145d9ca7ace2262be063a6fefd4916a1b80a2d
sformichella <sformichella@gmail.com>
2021-02-03 15:21:39 -0800

    NewsSearch scaffolding

--- src/container/NewsSearch/NewsSearch.jsx
+++ src/container/NewsSearch/NewsSearch.jsx
@@ -1,11 +1,31 @@
 import React, { Component } from "react";
+import Search from "../../presentational/Search/Search";
+import Articles from "../../presentational/Articles/Articles";
 
 export default class NewsSearch extends Component {
+
+  state = {
+    search: "",
+    articles: []
+  }
+
+  onSearchChange = () => {
+
+  }
+
   render() {
+
+    const { search, articles } = this.state;
+    const { onSearchChange } = this;
+
     return (
-      <div>
-        
-      </div>
+      <>
+        <Search 
+          search={ search }
+          onSearchChange={ onSearchChange }/>
+        <Articles 
+          articles={ articles }/>
+      </>
     );
   }
 }
---
+++ src/container/NewsSearch/NewsSearch.test.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import NewsSearch from "./NewsSearch";
+
+describe("NewsSearch", () => {
+  it("changes the articles when search is changed", () => {
+    render(<NewsSearch />);
+
+    
+  });
+});
