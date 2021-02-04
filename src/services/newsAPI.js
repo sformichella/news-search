bf732e3bb3b014f9b0d987186dfbcc5a0e59b502
sformichella <sformichella@gmail.com>
2021-02-03 18:56:50 -0800

    fix broken stuff I hope

--- src/services/newsAPI.js
+++ src/services/newsAPI.js
@@ -1,6 +1,6 @@
 const CORS = "https://cors-anywhere.herokuapp.com/";
+const API_KEY = "4281160a2a4540e598a5a0a53948cd28";
 const API_URL = `http://newsapi.org/v2/everything?apikey=${API_KEY}`;
-const API_KEY = "c9e579a31e5741049e20e6438b7b3291";
 
 const getArticles = search => {
   return fetch(CORS + API_URL + "&q=" + search, { headers: { origin: null } })
