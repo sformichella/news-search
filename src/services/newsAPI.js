9221f115ca13496f01497e69582a6fb278c485f9
sformichella <sformichella@gmail.com>
2021-02-03 18:32:47 -0800

    cors thing

--- src/services/newsAPI.js
+++ src/services/newsAPI.js
@@ -1,8 +1,9 @@
-const API_KEY = "c9e579a31e5741049e20e6438b7b3291";
+const CORS = "https://cors-anywhere.herokuapp.com/";
 const API_URL = `http://newsapi.org/v2/everything?apikey=${API_KEY}`;
+const API_KEY = "c9e579a31e5741049e20e6438b7b3291";
 
 const getArticles = search => {
-  return fetch(API_URL + "&q=" + search)
+  return fetch(CORS + API_URL + "&q=" + search, { headers: { origin: null } })
     .then(res => res.json())
     .then(json => json.articles);
 };
